ccadde7f55b6456de967427a2b513e58d7962aa5
Keith Isham <kisham8899@gmail.com>
2019-09-14 17:27:05 +0000

    Done.

--- models/user.rb
+++ models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   def say_name
     "my name is #{self.name}"
   end
+  #I hate The videos with Avi? Idk.. hes the host in every vid.. Its terrible. Learn video editing technques.. Not fucking hard. 
 end
