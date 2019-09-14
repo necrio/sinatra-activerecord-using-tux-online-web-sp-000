41b70768e9fb970decdb2c8f2b5b5d50342ceda0
Keith Isham <kisham8899@gmail.com>
2019-09-14 17:26:13 +0000

    Automatically backed up by Learn

--- models/user.rb
+++ models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   def say_name
     "my name is #{self.name}"
   end
+  #HA FUCK U... The guy that is in every video.. Avi? Fuck that guy. Literally the WORST. Anytime  I HAVE to watch his videos, I put them on mute. Learn some fucking video editing. You're taking nearly 10,000 dollars from every student. At least spend it on some fucking decent video editing training. Fucker. Die. 
 end
